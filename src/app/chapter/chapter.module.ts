e4d993d48392e56d9f023a462f2040e774620056
monis001 <mmonis@3clogic.com>
2019-05-02 19:50:28 +0530

    added resolver for chapter route

--- src/app/chapter/chapter.module.ts
+++ src/app/chapter/chapter.module.ts
@@ -4,9 +4,12 @@ import { ChapterComponent } from './chapter.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ChapterTheoryComponent } from './chapter-theory/chapter-theory.component';
 import { ChapterTaskComponent } from './chapter-task/chapter-task.component';
+import { ChapterResolver } from './chapter-resolver';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-    imports:[NgbModule,ChapterRouteModule],
+    imports:[NgbModule,ChapterRouteModule, HttpClientModule],
     declarations:[ChapterComponent,ChapterTheoryComponent,ChapterTaskComponent],
-    exports:[]
+    exports:[],
+    providers: [ChapterResolver]
 })
 export class ChapterModule {}

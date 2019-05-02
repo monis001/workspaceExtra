2b0a0fe643eab8c227cdb0eaec76588bfe38a90a
monis001 <mmonis@3clogic.com>
2019-05-02 19:01:10 +0530

    Header update

--- src/app/chapter/chapter.component.ts
+++ src/app/chapter/chapter.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from "@angular/core";
-
+declare var $ : any ;
 @Component({
     selector: 'app-chapter',
-    templateUrl: './chapter.component.html'
+    templateUrl: './chapter.component.html',
+    styleUrls:['./chapter.component.css']
 })
 export class ChapterComponent implements OnInit {
     ngOnInit(): void {
         console.log('From chapters')
+      
+//         const find = document.querySelector.bind(document);
+
+// find('#updateBtn').onclick = () => {
+//   // total is the circumference of the circle
+//   // aka 2 * π * r
+//   const total = 314;
+//   //   25% filled would be:
+//   // const value = total * .25
+//   const value =  * total;
+  
+  
+//   find('#circle').style.strokeDasharray = value + ' ' + total;
+
+// }
     }
 }

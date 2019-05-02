b5f4d7102b776ec01c1c9beeda3cb4475a9c6e66
monis001 <mmonis@3clogic.com>
2019-05-02 16:58:15 +0530

    Task component raw data

--- src/app/chapter/chapter-task/chapter-task.component.ts
+++ src/app/chapter/chapter-task/chapter-task.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-chapter-task',
-    templateUrl: './chapter-task.component.html'
+    templateUrl: './chapter-task.component.html',
+    styleUrls:['./chapter-task.component.css']
 })
 export class ChapterTaskComponent {
     

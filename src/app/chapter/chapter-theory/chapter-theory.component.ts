52a6258b5c9afe2f792db7728c51225c3d9f3eef
monis001 <mmonis@3clogic.com>
2019-05-02 16:24:28 +0530

    Chapter theory dummy update

--- src/app/chapter/chapter-theory/chapter-theory.component.ts
+++ src/app/chapter/chapter-theory/chapter-theory.component.ts
@@ -1,9 +1,25 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+declare var $: any ;
 @Component({
     selector:'app-chapter-theory',
-    templateUrl:'./chapter-theory.component.html'
+    templateUrl:'./chapter-theory.component.html',
+    styleUrls:['./chapter-theory.component.css']
 })
-export class ChapterTheoryComponent {
+export class ChapterTheoryComponent implements OnInit{
+    ngOnInit(): void {
+        var winHeight = $(window).height(),
+  docHeight = $(document).height(),
+  progressBar = $("progress"),
+  max,
+  value;
+max = docHeight - winHeight;
+progressBar.attr("max", max);
+
+$(document).on("scroll", function() {
+  value = $(window).scrollTop();
+  progressBar.attr("value", value);
+});
+
+    }
     
 }

146277641cdd81b51f4f1d492b1d966ec45f2c13
monis001 <mmonis@3clogic.com>
2019-05-02 15:56:30 +0530

    Componets updated

---
+++ src/app/chapter/chapter-task/chapter-task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-chapter-task',
+    templateUrl: './chapter-task.component.html'
+})
+export class ChapterTaskComponent {
+    
+}
---
+++ src/app/chapter/chapter-theory/chapter-theory.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-chapter-theory',
+    templateUrl:'./chapter-theory.component.html'
+})
+export class ChapterTheoryComponent {
+    
+}

1b41611e737d4c461b8edd53582f5f23db657421
swapnil171 <sgupta@3clogic.com>
2019-05-03 13:28:16 +0530

    theory markdown added. Tasks with dummy data implemented.

--- src/app/chapter/chapter-task/chapter-task.component.ts
+++ src/app/chapter/chapter-task/chapter-task.component.ts
@@ -1,10 +1,43 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-chapter-task',
     templateUrl: './chapter-task.component.html',
-    styleUrls:['./chapter-task.component.css']
+    styleUrls: ['./chapter-task.component.css']
 })
 export class ChapterTaskComponent {
-    
+
+  public tasks: any[] = [{
+    'id': 1,
+    'chapter_id': 101,
+    'question': 'Create a form action',
+    'description': 'Add a form action name Test',
+    'status': 'Completed',
+
+  },
+    {
+      'id': 2 ,
+      'chapter_id': 101,
+      'question': 'Disable a newly created form action',
+      'description': 'Add a form action name Test',
+      'status': 'InProgress',
+
+    },
+    {
+      'id': 3 ,
+      'chapter_id': 101,
+      'question': 'Make your form action global',
+      'description': 'Apply a newly created form action on all forms.',
+      'status': 'InProgress',
+
+    },
+    {
+      'id': 4 ,
+      'chapter_id': 101,
+      'question': 'Enable a newly created form action',
+      'description': 'Add a form action name Test',
+      'status': 'InProgress',
+
+    }
+  ];
 }
